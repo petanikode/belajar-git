016ac7c8d2b16592799fa9b4a2e6f0db992ecfcf
dhevanthareza <dhevanmuhamad@gmail.com>
2019-10-02 10:01:17 +0700

    add pwa feature

---
+++ sw.js
@@ -0,0 +1,32 @@
+const version = "0.1.1";
+const cacheName = `belajar-git-${version}`;
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        `/`,
+        `/index.html`,
+        `/login.html`,
+        `/register.html`,
+        `/about.html`,
+        '/contact.html',
+        `/ico.png`
+      ])
+          .then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
