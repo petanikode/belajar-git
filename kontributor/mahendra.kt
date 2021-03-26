d60382958f8ba6d723bfeb8bb79b5b33b5a608b2
hendrasob <mahendraagusti@gmail.com>
2021-03-26 18:24:38 +0700

    kontribusi mahendra

---
+++ kontributor/mahendra.kt
@@ -0,0 +1,8 @@
+object Mahendra {
+
+ val name = "Mahendra"
+ val goals = "Belajar bersama-bersama dengan para programmer Indonesia"
+ val learning = "Belajar tentang Kotlin"
+ val github = "https://github.com/hendrasob"
+
+}
