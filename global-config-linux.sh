2959faec831347a95b65a7246cf77725e61ebfc5
Prayoga Eka Ardiansyah <prayogaea01@gmail.com>
2022-03-05 08:44:41 +0700

    Create global-config-linux.sh
    Menambahkan global-config-linux.sh

---
+++ global-config-linux.sh
@@ -0,0 +1,4 @@
+git global setup Linux
+-----------------------------------------------------------------------
+git config --global user.name "username"
+git config --global user.email "example@gmail.com"
